8cefeb267afa2d2975af0e35cf903580e7be7b86
Gayana-19 <64000886+Gayana-19@users.noreply.github.com>
2022-10-17 11:13:10 +0530

    Add files via upload

---
+++ assign 1/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeees
+{
+    public class Employee
+    {
+        public string Name;
+        public char Gender;
+        public string DateOfBirth;
+        public int Age;
+        public string Password;
+        public string Email;
+        public int age
+        {
+            get { return Age; }
+        }
+
+        public string password
+        {
+            set { Password = value; }
+        }
+        public string EmpId;
+
+        // Default constructor
+        public Employee()
+        {
+
+        }
+
+        // Parameterized constructor
+        
+        public  Employee(string Name, char Gender, string DateOfBirth, int Age,
+               string Email, string Password, string EmpId)
+        {
+            this.Name = Name;
+            this.Gender = Gender;
+            this.DateOfBirth = DateOfBirth;
+            this.Age = Age;
+            this.Email = Email;
+            this.Password = Password;
+            this.EmpId = EmpId;
+        }
+
+
+        // Accept Details
+        public void EmpAccept(string Name, char Gender, string DateOfBirth,
+                int Age, string Email, string Password, string EmpId)
+        {
+            this.Name = Name;
+            this.Gender = Gender;
+            this.DateOfBirth = DateOfBirth;
+            this.Age = Age;
+            this.Email = Email;
+            this.Password = Password;
+            this.EmpId = EmpId;
+        }
+
+        // Display Details
+        public void EmpDisp(string EmpId)
+        {
+            Console.WriteLine("Name : " + this.Name);
+            Console.WriteLine("Gender : " + this.Gender);
+            Console.WriteLine("DateOfBirth : " + this.DateOfBirth);
+            Console.WriteLine("Age : " + this.Age);
+            Console.WriteLine("Email : " + this.Email);
+            Console.WriteLine("Password : " + this.Password);
+            Console.WriteLine("EmpId : " + this.EmpId);
+        }
+    }
+
+}
+
---
+++ assign 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Main Program
+using employeees;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating Tech Employee Object and assigning values
+        TechEmployee emp = new TechEmployee();
+        emp.AcceptTechEmployee("Employee", 'M', "1/1/2001", 21,
+            "employee123@company.com", "passwd", "empid01",
+            "SDE", "Manager", 1);
+        Console.WriteLine("Employee Details\n");
+        emp.TechEmpDisp("empid01");
+
+        Console.WriteLine("\n");
+
+        // Create Manager Object and assigning values
+        Manager mgr = new Manager();
+        mgr.AcceptManager("Manager", 'F', "2/2/2000", 22,
+            "manager234@company.com", "passwd01", "mgrid01",
+            "Software Engineering", "Tech Lead", 8, 2);
+        Console.WriteLine("Manager Details\n");
+        mgr.ManagerDisp("mgrid01");
+
+    }
+}
