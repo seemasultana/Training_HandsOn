a30575d0f5573a8281cff822d1c209876ec02006
sandhyaj2000 <82196023+sandhyaj2000@users.noreply.github.com>
2022-10-17 14:15:49 +0530

    Add files via upload

---
+++ sandhya_oops_new/Bank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandhya_oops_new
+{
+
+
+    class Bank
+    {
+        public int balance;
+        public string name;
+        public string password;
+        public int accno;
+
+        public void withdrawal(int money)
+        {
+            if (money < balance)
+            {
+                balance = balance - money;
+                Console.WriteLine("YOUR BALANCE AMOUNT IS :" + balance);
+            }
+            else
+            {
+                Console.WriteLine("YOU HAVE INSUFFICIENT BALANCE");
+            }
+        }
+        public void deposit(int money)
+        {
+            balance = balance + money;
+            Console.WriteLine("YOU HAVE DEPOSITED : " + money);
+        }
+        public void checkbalance()
+        {
+            Console.WriteLine("your current balance is  :" + balance);
+        }
+        public void display()
+        {
+            Console.WriteLine("ACCOUNT NO :" + accno);
+            Console.WriteLine("ACCOUNT NAME :" + name);
+            Console.WriteLine("ACCOUNT BALANCE :" + balance);
+        }
+        public void openaccount(int accno, string name, string password, int bal)
+        {
+            this.accno = accno;
+            this.name = name;
+            this.password = password;
+            this.balance = bal;
+        }
+    }
+}
---
+++ sandhya_oops_new/ExecuteBank.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandhya_oops_new
+{
+    
+    internal class Executebank
+    {
+        static void Main(string[] args)
+        {
+            Bank b1 = new Bank();
+            Bank b2 = new Bank();
+            Bank b3 = new Bank();
+            b1.openaccount(551, "ram", "ram@123", 3000);
+            b2.openaccount(992, "ria", "ria#123", 2000);
+            b3.openaccount(993, "tina", "tina@123", 4000);
+            int c;
+            int c1 = 0;
+            string p;
+
+            do
+            {
+                Console.WriteLine("ENTER YOUR LOGIN DETAILS");
+                Console.WriteLine("ENTER USER NAME");
+                string n1 = Console.ReadLine();
+                Console.WriteLine("ENTER PASSWORD");
+                string p1 = Console.ReadLine();
+
+                if (b1.name == n1 && b1.password == p1)
+                {
+                    Console.WriteLine("WELCOME " + b1.name);
+
+                    break;
+
+                }
+                else if (b2.name == n1 && b2.password == p1)
+                {
+                    Console.WriteLine("WELCOME " + b2.name);
+
+                    break;
+                }
+                else if (b3.name == n1 && b3.password == p1)
+                {
+                    Console.WriteLine("WELCOME " + b3.name);
+                }
+                else
+                {
+                    Console.WriteLine("INVALID LOGIN DETAILS");
+                    Console.WriteLine("PLEASE RETRY");
+                    c1++;
+                }
+            } while (c1 < 2);
+
+
+
+
+
+
+            do
+            {
+                Console.WriteLine(" 1.DEPOSIT MONEY");
+                Console.WriteLine(" 2.WITHDRAW MONEY");
+                Console.WriteLine(" 3.CHECK BALANCE");
+                Console.WriteLine(" 4.DISPLAY DETAILS ");
+                Console.WriteLine("ENTER  AN OPTION");
+
+
+                c = Convert.ToInt32(Console.ReadLine());
+                switch (c)
+                {
+                    case 1:
+                        Console.WriteLine("enter amount to be deposited");
+                        int m = Convert.ToInt32(Console.ReadLine());
+                        b1.deposit(m);
+                        break;
+                    case 2:
+                        Console.WriteLine("enter amount to be withdrawed");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        b1.withdrawal(b);
+                        break;
+                    case 3:
+                        b1.checkbalance();
+                        break;
+                    case 4:
+                        b1.display();
+                        break;
+                    default:
+                        Console.WriteLine("INVALID OPTION");
+                        break;
+
+
+                }
+                Console.WriteLine("DO YOU WANT TO CONTINUE WITH OTHER BANKING OPTIONS(yes/no");
+                p = Console.ReadLine();
+            } while (p != "no");
+
+        }
+    }
+}
