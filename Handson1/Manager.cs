11a5ba4d7c74082de973be442e58b643b9e2d4aa
sunanda42 <102569686+sunanda42@users.noreply.github.com>
2022-10-17 14:17:54 +0530

    Add files via upload

---
+++ Handson1/Manager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sunanda
+{
+    class Manager : Employee
+    {
+        public string Department = "Engineering";
+        public string Title = "Associate Engineer";
+        public int Reportees = 50;
+        public int TotalExp = 10;
+
+        public Manager()
+        {
+            Console.WriteLine("Default constructor is called here");
+        }
+
+        public Manager(string department, string title, int reportees, int totalExp)
+        {
+            this.Department = department;
+            this.Title = title;
+            this.Reportees = reportees;
+            this.TotalExp = totalExp;
+            Console.WriteLine("Parameterised constructor is called here");
+        }
+
+        public void ddisplay()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("     Manager Details      ");
+            Console.WriteLine("Department=" + Department);
+            Console.WriteLine("Title=" + Title);
+            Console.WriteLine("Reportees=" + Reportees);
+            Console.WriteLine("Total years of experience=" + TotalExp);
+        }
+    }
+}
---
+++ Handson1/TechEmployees.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sunanda
+{
+
+    class TechEmployees : Employee
+    {
+        public string Designation = "Associate Engineer";
+        public string ReportingTo = "Zulquar Siddique";
+        public int TotalExp = 0;
+    
+        public TechEmployees()
+        {
+            Console.WriteLine("Default constructor is called here");
+        }
+        public TechEmployees(string Designation, string ReportingTo, int TotalExp)
+        {
+            this.Designation = Designation;
+            this.ReportingTo = ReportingTo;
+            this.TotalExp = TotalExp;
+            Console.WriteLine("Parameterised constructor is called here");
+        }
+
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   Employee Details    ");
+            Console.WriteLine("Designation=" + Designation);
+            Console.WriteLine("ReportingTo= + Title");
+            Console.WriteLine("Reportees=" + ReportingTo);
+            Console.WriteLine("Total experience=" + TotalExp);
+        }
+    
+    }
+}
