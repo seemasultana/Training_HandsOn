653c414dc750024311ff01dcd95bf18dd2894f02
sandhyaj2000 <82196023+sandhyaj2000@users.noreply.github.com>
2022-10-17 11:27:54 +0530

    Add files via upload

---
+++ project1/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Gender;
+        public string DateOfBirth;
+        public int Age;
+        public string Email;
+        public string Password;
+        public int EmpId;
+
+        public  Employee()
+        {
+            
+        }
+
+        public Employee(string name, string gender , string dob , int age , string email , string password, int empid)
+        {
+            this.Name = name;
+            this.Gender = gender;   
+            this.Email = email;
+            this.DateOfBirth = dob;
+            this.Age = age;
+            this.Email = email;
+            this.Password = password;
+            this.EmpId = empid;
+        }
+
+        public  virtual void  acceptdetails()
+        {
+            Console.WriteLine("WELCOME TO HARMAN");
+            Console.WriteLine("enter your name");
+            string n = Console.ReadLine();
+            Console.WriteLine("enter your employee ID");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your gender");
+            string g = Console.ReadLine();
+            Console.WriteLine("enter your email");
+            string e = Console.ReadLine();
+            Console.WriteLine("enter your password");
+            string p = Console.ReadLine();
+            Console.WriteLine("enter your age");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your DOB");
+            string d = Console.ReadLine();
+
+
+        }
+        public virtual void displaydetails()
+        {
+            Console.WriteLine("NAME : " + Name);
+            Console.WriteLine("EMPLOYEE ID : " + EmpId);
+            Console.WriteLine("EMAIL - ID : " + Email);
+            Console.WriteLine("DATE OF BIRTH : " + DateOfBirth);
+            Console.WriteLine("GENDER : " + Gender);
+            Console.WriteLine("AGE : " + Age);
+
+
+        }
+        
+    }
+}
---
+++ project1/TechEmployees.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment1
+{
+    internal class TechEmployees : Employee
+    {
+        public string Designation;
+        public string ReportingTo;
+        public int TotalExp;
+        public TechEmployees()
+        {
+            
+        }
+
+        public TechEmployees(string name, string gender, string dob, int age, string email, string password, int empid, string designation, string reportingto, int totalexp)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Email = email;
+            this.DateOfBirth = dob;
+            this.Age = age;
+            this.Email = email;
+            this.Password = password;
+            this.EmpId = empid;
+            this.Designation = designation;
+            this.ReportingTo = reportingto;
+            this.TotalExp = totalexp;
+        }
+
+        public override void acceptdetails()
+        {
+
+            base.acceptdetails();
+            Console.WriteLine("enter the reporting manager name");
+            string r1 = Console.ReadLine();
+            Console.WriteLine("enter the designation");
+            string d1 = Console.ReadLine();
+            Console.WriteLine("enter the years of experience");
+            int e1 = Convert.ToInt32(Console.ReadLine());
+            
+
+            
+        }
+
+        public override void displaydetails()
+        {
+            base.displaydetails();
+            Console.WriteLine("DESIGNATION : " + Designation);
+            Console.WriteLine("EXPERIENCE(YEARS) : " + TotalExp);
+            Console.WriteLine("REPORTING TO : " + ReportingTo);
+        }
+
+
+
+    }
+}
