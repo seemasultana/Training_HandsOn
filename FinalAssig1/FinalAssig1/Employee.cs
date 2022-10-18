6a613aa04343da6a166ac451658df343bdb42b1d
Bhoomika216 <85291052+Bhoomika216@users.noreply.github.com>
2022-10-18 13:55:49 +0530

    Add files via upload

---
+++ FinalAssig1/FinalAssig1/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssig1
+{
+    public class Employee
+    {
+        public string Name;
+        public string Gender;
+        public string DateOfBirth;
+        public int Age;
+        public string Email;
+        public int EmpId;
+        public string Password;
+
+        /*read only
+        public int Agee
+        {
+            get { return Age; }
+        }
+
+        //write only
+        public string Passwordd
+        {
+            set { Password = value; }
+        }*/
+
+        //Default Constructor
+        public Employee()
+        {
+            Console.WriteLine("\nDefault Constructor");
+        }
+
+        //Parameterized Constructor
+        public Employee(string name, string gender, string dateOfBirth, int age, string email, string password, int empId)
+        {
+            Name = name;
+            Gender = gender;
+            DateOfBirth = dateOfBirth;
+            Age = age;
+            Email = email;
+            EmpId = empId;
+            Console.WriteLine("Parameterized constructor");
+        }
+
+        public void EmpDisplay()
+        {
+            Console.WriteLine("\nEmployee details are displayed below: ");
+            Console.WriteLine("Employee Name: " + Name);
+            Console.WriteLine("Employee Gender: " + Gender);
+            Console.WriteLine("Employee DOB: " + DateOfBirth);
+            Console.WriteLine("Employee Age: " + Age);
+            Console.WriteLine("Employee Email: " + Email);
+            Console.WriteLine("Employee ID: " + EmpId);
+        }
+    }
+}
---
+++ FinalAssig1/FinalAssig1/TechEmployees.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssig1
+{
+    public class TechEmployees : Employee
+    {
+        public string Designation;
+        public string ReportingTo;
+        public double TotalExp;
+
+        public TechEmployees()
+        {
+            Console.WriteLine("\nDefault Constructor");
+        }
+
+        public TechEmployees(string name, string gender, string dateOfBirth, int age, string email, string password, int empId, string designation, string reportingTo, double totalExp)
+        {
+            Name = name;
+            Gender = gender;
+            DateOfBirth = dateOfBirth;
+            Age = age;
+            Email = email;
+            EmpId = empId;
+            Designation = designation;
+            ReportingTo = reportingTo;
+            TotalExp = totalExp;
+            Console.WriteLine("Parameterized constructor");
+        }
+
+        public void TechEmpDisplay()
+        {
+            EmpDisplay();
+            Console.WriteLine("\nTech Employee details are displayed below: ");
+            Console.WriteLine("Designation: " + Designation);
+            Console.WriteLine("Reporting To: " + ReportingTo);
+            Console.WriteLine("Total Experience in years: " + TotalExp);
+        }
+    }
+}
