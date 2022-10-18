59d2fc9f79d9431e1f0a45072fc837cf442ce2cc
Gayana-19 <64000886+Gayana-19@users.noreply.github.com>
2022-10-18 14:41:22 +0530

    Add files via upload

---
+++ assign 1/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeees
+{
+    public class Employee
+    {
+        public string Name;
+        public char Gender;
+        public string DateOfBirth;
+        public int Age;
+        public string Password;
+        public string Email;
+        public int age
+        {
+            get { return Age; }
+        }
+
+        public string password
+        {
+            set { Password = value; }
+        }
+        public int EmpId;
+
+        // Default constructor
+        public Employee()
+        {
+
+        }
+        // Parameterized constructor
+        public  Employee(string name, char gender, string dateOfBirth, int age,
+               string email, string password, int empId)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.DateOfBirth = dateOfBirth;
+            this.Age = age;
+            this.Email = email;
+            this.Password = password;
+            this.EmpId = empId;
+        }
+        public void EmpAccept(string name, char gender, string dateOfBirth,
+                int age, string email, string password, int empId)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.DateOfBirth = dateOfBirth;
+            this.Age = age;
+            this.Email = email;
+            this.Password = password;
+            this.EmpId = empId;
+        }
+        public void EmpDisp()
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Gender : " + this.Gender);
+            Console.WriteLine("DateOfBirth : " + this.DateOfBirth);
+            Console.WriteLine("Age : " + this.Age);
+            Console.WriteLine("Email : " + this.Email);
+            Console.WriteLine("Password : " + this.Password);
+            Console.WriteLine("EmpId : " + this.EmpId);
+        }
+    }
+
+}
+
---
+++ assign 1/Manager.cs
@@ -0,0 +1,65 @@
+﻿using employeees.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeees
+{
+    public class Manager : Employee
+    {
+
+        public string Department;
+        public string Title;
+        public int Reportees;
+        public int TotalExp;
+        public Manager()//default constructor
+        {
+
+        }
+        public Manager(string name, char gender, string dateOfBirth, int age,
+                string email, string password, int empId,
+                string department, string title,
+                int totalExp)
+        {
+            this.EmpAccept(name, gender, dateOfBirth, age, email, password, empId);
+            this.Department = department;
+            this.Title = title;
+            this.TotalExp = totalExp;
+        }
+        public void ManagerDisp()
+        {
+            Console.WriteLine("...Manager details...\n");
+            EmpDisp();
+            Console.WriteLine("Department : " + this.Department);
+            Console.WriteLine("ReportingTo : " + this.Title);
+            Console.WriteLine("Reportees :");
+            Mlist();
+            Console.WriteLine("TotalExp : " + this.TotalExp);
+        }
+        public void Mlist()
+        {
+            report emp1 = new report()
+            {
+                Eid = 990,
+                EName = "gayana",
+                Eage = 22
+            };
+            report emp2 = new report()
+            {
+                Eid = 991,
+                EName = "abc",
+                Eage = 23
+            };
+            List<report> lst = new List<report>();
+            lst.Add(emp1);
+            lst.Add(emp2);
+            foreach (report r in lst)
+            {
+                Console.WriteLine("Eid={0}\nEName={1}\nEage={2}\n", r.Eid, r.EName, r.Eage);
+            }
+        }
+    }
+    }
+
