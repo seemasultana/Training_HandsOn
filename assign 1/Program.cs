4d68f6d123ad5a21573e86f0af4231dbceb5415c
Gayana-19 <64000886+Gayana-19@users.noreply.github.com>
2022-10-18 14:16:47 +0530

    Add files via upload

---
+++ assign 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using employeees;
+class Program
+{
+    static void Main(string[] args)
+    {
+        TechEmployee emp = new TechEmployee("Employee", 'M', "1/1/2001", 21,
+            "employee123@company.com", "passwd", 990,
+            "SDE", "Manager", 1);
+        Manager mgr = new Manager("Manager", 'F', "2/2/2000", 22,
+            "manager234@company.com", "passwd01", 991,
+            "Software Engineering", "Tech Lead", 2);
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("enter the employee id:");
+            int empid = Convert.ToInt32(Console.ReadLine());
+            if (empid == emp.EmpId)
+            {
+                emp.TechEmpDisp();
+
+            }
+            else if (empid == mgr.EmpId)
+            {
+                mgr.ManagerDisp();
+            }
+            else
+            {
+                Console.WriteLine("invalid!");
+
+            }
+        }
+    }
+
+}
---
+++ assign 1/model/report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeees.model
+{
+    class report
+    {
+        public int Eid { get; set; }
+        public string EName { get; set; }
+        public int Eage { get; set; }
+    }
+
+   
+}
