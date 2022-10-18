d872fe91cde067a4273a4db7f7e9aa9cf18edb5a
RishabhHarman04 <101667621+RishabhHarman04@users.noreply.github.com>
2022-10-18 23:56:47 +0530

    Add files via upload

---
+++ Opps Properties Folder/CalculatorClass.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Calculator
+{
+    public class Arithmetic_Opeartion
+    {
+        public int Add(int a, int b)
+            { return a + b; }
+        private int Sub(int a, int b)
+            { return a - b; }
+        protected int Mult(int a, int b)
+            { return a * b; }
+        internal int Div(int a, int b)
+            { return a / b; }
+        internal protected int Mod(int a, int b)
+            { return a % b; }
+    }
+    public  class Extra_Operation:Arithmetic_Opeartion
+        {
+        public int Add(int a, int b, int c)
+        { return a + b + c; }
+        public void MultTest()
+            {
+            System.Console.WriteLine(Mult(2,3));
+            }
+        
+        }
+}
+// Inheritance, Overloading(Polymorphism),Encapsulation
