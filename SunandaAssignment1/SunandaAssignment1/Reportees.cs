d189f90b61e5edc0844c2a9a68e9db76bf65d065
sunanda42 <102569686+sunanda42@users.noreply.github.com>
2022-10-19 11:21:37 +0530

    Add files via upload

---
+++ SunandaAssignment1/SunandaAssignment1/Reportees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunandaAssignment1
+{
+     class Reportees
+    {
+        public string ReporteeName;
+        public string ReporteeGender;
+        public string ReporteeEmpId;
+
+    }
+}
