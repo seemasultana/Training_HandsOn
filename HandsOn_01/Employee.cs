ec91a8fc06ce6d20e1f41d3a0f1c207d18d31511
sunanda42 <102569686+sunanda42@users.noreply.github.com>
2022-10-19 10:31:06 +0530

    Add files via upload

---
+++ HandsOn_01/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sunanda
+{
+    class Employee
+    {
+        public string Name;
+        public string Gender;
+        public string DateOfBirth;
+        public int Age;
+        public string Email;
+        public string Password;
+        public int EmpId;
+
+        public int Age1
+        {
+            get { return Age; }
+        }
+
+        public string password1
+        {
+            set { Password = value; }
+        }
+
+        public Employee()
+        {
+
+        }
+
+
+        public Employee(string Nm, string Gd, string DOB, int Ag, string Eml, string Pd, int EI)
+        {
+            Name = Nm;
+            Gender = Gd;
+            DateOfBirth = DOB;
+            Age = Ag;
+            Email = Eml;
+            Password = Pd;
+            EmpId = EI;
+           // Console.WriteLine("Parameterised constructor is called here");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Name=" + Name);
+            Console.WriteLine("Gender=" + Gender);
+            Console.WriteLine("DateOfBirth=" + DateOfBirth);
+            Console.WriteLine("Age=" + Age);
+            Console.WriteLine("Email=" + Email);
+            Console.WriteLine("Password=" + Password);
+            Console.WriteLine("EmpId=" + EmpId);
+        }
+
+
+        
+
+
+
+
+    }
+}
---
+++ HandsOn_01/Reportees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sunanda
+{
+    public class Reportees
+    {
+        public string ReporteeName;
+        public string ReporteeGender;
+        public string ReporteeEmpId;
+
+
+    }
+}
