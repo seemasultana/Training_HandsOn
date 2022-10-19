8ac51aeda0ca63b7dafbe4d6e24b671d83a96011
sunanda42 <102569686+sunanda42@users.noreply.github.com>
2022-10-19 11:12:37 +0530

    Add files via upload

---
+++ HandsOn-01/SunandaAssignment1/SunandaAssignment1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunandaAssignment1
+{
+    class program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee("Sunanada", "Female", "01-3-2000", 21, "abc@gmail.com", "efef4", 99018000); ;
+            TechEmployees emp = new TechEmployees("vikas", "male", "2-4-2010", 13, "viki@gmail.com", "fgrg", 99012, "Software Engineer", "Manager", 5);
+            Manager emp1 = new Manager("revanth", "male", "25-7-20001", 20, "revanth@gmail.com", "asdf", 990180, "Enginnering", "Associate Engineer", 50, 10);
+            Console.WriteLine("Enter the Employee Id : ");
+
+
+            int empid = Convert.ToInt32(Console.ReadLine());
+            if (empid == employee.EmpId)
+            {
+                employee.Display();
+
+            }
+            else if (empid == emp.EmpId)
+            {
+                emp.display();
+            }
+            else
+            {
+                emp1.ddisplay();
+
+            }
+            Console.WriteLine("All the information is displayed");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
